b435e5ac0a6d8925092891d943d569f906ec6d87
vali-aidman <147526112+vali-aidman@users.noreply.github.com>
2023-11-07 11:02:31 +0530

    Add Mayer's CSS and JS to test DOM manipulation
    Added Mayer's reset CSS to allow for browser independence but
    the page is looking worse. Might remove it later. Also Added
    javascript to modify the body background color and font size
    (to check for inheritance). The color changes from pink to green
    on mouse click.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-console.log("Hello, World!")
+console.log("Hello, World!");
+let num1 = 10;
+let num2 = num1 + 20;
+const PI = 3.14;
+num2 = num1*2;
+somestring = "Avinash";
+console.log(num1,num2);
+console.log(somestring);
+console.log(PI);
+console.log(1=="1");
+console.log(1==="1");
+let go = null;
+console.log(typeof "8");
+const root = document.querySelector('body');
+console.log(root);
+root.addEventListener('click', function () {
+  root.style.background = 'blue';
+  root.style.color = 'blue';
+  root.style.backgroundColor = 'green';
+  root.style.fontSize = '20px';
+});
+
